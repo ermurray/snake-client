19f690a9090d0d3901bc0230e6d75a59751c94e9
ermurray <ermurray31@gmail.com>
2021-02-10 16:59:31 +0000

    refactored and moved input functions to input.js

--- play.js
+++ play.js
@@ -1,27 +1,7 @@
-const net = require('net');
-const { stdin } = require('process');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting  ...');
 connect();
 
-
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput();
-  return stdin;
-};
-const handleUserInput = function() {
-  stdin.on('data', (key) => {
-    if(key === '\u0003') {
-      process.exit();
-    }
-  });
-
-};
 setupInput();
