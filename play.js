d22e6f5a1ef97cdb523a772efb579196599af3ec
ermurray <ermurray31@gmail.com>
2021-02-10 08:23:37 +0000

    added handle user input

--- play.js
+++ play.js
@@ -1,5 +1,26 @@
 const net = require('net');
+const { stdin } = require('process');
 const { connect } = require('./client');
 
 console.log('Connecting  ...');
 connect();
+
+
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    
+  });
+  
+
+  return stdin;
+};
+setupInput();
