039f724ece34af7c8c642c98064e75e8161eaf97
ermurray <ermurray31@gmail.com>
2021-02-10 16:51:42 +0000

    changed function definition of handleUserInput to outside SetUpInput and called handleUserInput inside SetupInput.

--- play.js
+++ play.js
@@ -13,14 +13,15 @@ const setupInput = function() {
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  const handleUserInput = stdin.on('data', (key) => {
-    if (key === '\u0003') {
+  handleUserInput();
+  return stdin;
+};
+const handleUserInput = function() {
+  stdin.on('data', (key) => {
+    if(key === '\u0003') {
       process.exit();
     }
-    
   });
-  
 
-  return stdin;
 };
 setupInput();
